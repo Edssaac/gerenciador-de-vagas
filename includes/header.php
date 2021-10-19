6a37b3e752953b1faa1489711954981a1c9f2a72
Edssaac <edssaac@outlook.com>
2021-10-19 11:09:39 -0300

    ...

--- includes/header.php
+++ includes/header.php
@@ -1,26 +1,25 @@
 <!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <html lang="en">
+    <head> 
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-    <!-- Breakpoints Implementation -->
-    <link rel="stylesheet" href="./suporte/breakpoints.css">
+      <!-- Breakpoints Implementation -->
+      <link rel="stylesheet" href="./suporte/breakpoints.css">
 
-    <!-- PAGE ICON -->
-    <link rel="shortcut icon" href="https://img.icons8.com/plasticine/100/000000/find-matching-job.png">
+      <!-- PAGE ICON -->
+      <link rel="shortcut icon" href="https://img.icons8.com/plasticine/100/000000/find-matching-job.png">
 
-    <title>Vagas</title>
-  </head>
-  <body class="bg-dark text-light">
+      <title>Vagas</title>
+    </head>
+    <body class="bg-dark text-light">
 
     <div class="container">
-
       <div class="jumbotron bg-danger text-center">
-        <h1>Classificados Fatec</h1>
-        <p>Gerencie suas ofertas de negócio.</p>
-      </div>
+      <h1>Classificados Fatec</h1>
+      <p>Gerencie suas ofertas de negócio.</p>
+    </div>
